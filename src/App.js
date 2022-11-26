8b20e40e1068ca5bc081d10d4297a8a42ad34a05
518651 <69994181+518651@users.noreply.github.com>
2022-11-26 22:20:46 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter , Routes, Route } from 'react-router-dom';  // 使用BrowserRouter
+import { Box } from '@mui/material';
+
+const App = () =>  (
+    <BrowserRouter>
+        <Box sx={{background: '#000'}}>
+            <Navbar/>
+            <Routes>
+                <Route path="/" exact element={<Feed/>}/>
+                <Route path="/video/:id" element={<VideoDetail/>}/>
+                <Route path="/channel/:id" element={<ChannelDetail/>}/>
+                <Route path="/search/:searchTerm" element={<SearchFeed/>}/>
+            </Routes>
+        </Box>
+    </BrowserRouter>
+)
+
+
+export default App
